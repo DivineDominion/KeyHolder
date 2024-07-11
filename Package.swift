8aa1b9c0f76af2532f8e0efd67303faab9781006
Christian Tietze <me@christiantietze.de>
2024-07-11 18:44:47 +0200

    set SwiftPM to use hotfix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["KeyHolder"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Clipy/Magnet", .upToNextMinor(from: "3.4.0")),
+        .package(url: "https://github.com/DivineDominion/Magnet", .branch("hotfix2021")),
     ],
     targets: [
         .target(
